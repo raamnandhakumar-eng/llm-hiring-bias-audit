de0d3d46c36336e862272ec503a1176c262c9f83
Raam Nandhakumar <raam.nandhakumar@gmail.com>
2026-07-25 17:52:13 -0400

    Add validation tests

--- tests/test_external_preregistration.py
+++ tests/test_external_preregistration.py
@@ -47,5 +47,3 @@ def test_live_gate_rejects_non_https_url(monkeypatch):
     monkeypatch.setenv("EXTERNAL_PREREGISTRATION_URL", "http://osf.io/abc12")
     with pytest.raises(RuntimeError, match="permanent HTTPS registration URL"):
         assert_external_preregistration(_config())
-
-# label refresh
